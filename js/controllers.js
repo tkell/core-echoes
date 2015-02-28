7859b96b7bd97885ae6e75adf3748a9d4b8cbe1c
tkell <thor.kell@mail.mcgill.ca>
2015-02-28 15:38:49 -0500

    To pitch and rhythm

--- js/controllers.js
+++ js/controllers.js
@@ -14,8 +14,8 @@ coreEchoesApp.controller('echoController', function ($scope) {
     var tempo = 128;
     var beatTime = 60 / 128;
 
-    synth1.setOscType('triangle');
-    synth2.setOscType('square');
+    synth1.setOscType('sine');
+    synth2.setOscType('sine');
     synth3.setOscType('sine');
 
     // More synth quality here!
@@ -24,30 +24,103 @@ coreEchoesApp.controller('echoController', function ($scope) {
     synth2.toMaster();
     synth3.toMaster();
 
-    noteData = [{'note': 'C4', 'duration': 0.5, 'repetition':  beatTime / 4},
+    // Initial notes
+
+    noteData = [{'note': 'C4', 'duration': 0.2, 'repetition':  beatTime},
             {'note': 'G4', 'duration': 0.5, 'repetition':  beatTime},
             {'note': 'C5', 'duration': 0.5, 'repetition':  beatTime / 6}
         ];
 
-    // Let's convert things here
-    for (var i = 0; i < $scope.echoes.length; i++) {
-        
+    var currentIP = $scope.echoes[0].ip;
+    currentIP = currentIP.split('.');
+    // pad ips
+    for (var i = 0; i < currentIP.length; i++) {
+        if (currentIP[i].length == 2) {
+            currentIP[i] = '0' + currentIP[i];
+        } 
+        else if (currentIP[i].length == 1) {
+            currentIP[i] = '00' + currentIP[i];
+        }
     }
 
+    var getProbabilties = function(ipString) {
+        var probabilities = [];
+        var totalProb = 0;
+        for (var i = 0; i < ipString.length; i++) {
+            probabilities[i] = parseInt(ipString[i]);
+            totalProb = totalProb + probabilities[i];
+        }
 
+        for (var i = 0; i < probabilities.length; i++) {
+            probabilities[i] = probabilities[i] / totalProb;
+        }
+
+        return probabilities;
+    }
+
+    var getPitches = function(ipArray) {
+        var pitches = [];
+        var convertToLetters = ['C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4'];
+
+        for (var i = 0; i < ipArray.length; i++) {
+            var pitch = 0;
+            for (var j = 0; j < ipArray[i].length; j++) {
+                pitch = pitch + parseInt(ipArray[i][j]);
+            }
+            pitches[i] = convertToLetters[pitch % 12];
+        }
+        return pitches;
+    }
+
+    var choose = function(probabilities, array) {
+        var res = Math.random();
+        if (res < probabilities[0]) {
+            index = array[0]
+        } else if (res >= probabilities[0] && res < probabilities[1]) {
+            index = array[1]
+        } else {
+            index = array[2]
+        }
+        return index;
+    }
+
+    var getRhythms = function(ipArray) {
+        var intervals = [];
+        for (var i = 0; i < ipArray.length; i++) {
+            var interval = 0;
+            for (var j = 0; j < ipArray[i].length; j++) {
+                interval = interval + parseInt(ipArray[i][j]);
+            }
+            intervals[i] = interval / 4;
+        }
+        return intervals;   
+    }
+
+    var pitchProb = getProbabilties(currentIP[0]);
+    var pitches = getPitches(currentIP.slice(1, 4));
+    noteData[0].note = choose(pitchProb, pitches);
+
+    var rhythmProb = getProbabilties(currentIP[1]);
+    var rhythms = getRhythms([currentIP[0]].concat(currentIP.slice(2, 4)))
+    noteData[0].repetition = choose(rhythmProb, rhythms); 
+
+
+    console.log(currentIP);
+    console.log(pitchProb, pitches, noteData[0].note);
+    console.log(rhythmProb, rhythms, noteData[0].repetition);
 
 
     Tone.Transport.setInterval(function(time) {
         synth1.triggerAttackRelease(noteData[0].note, noteData[0].duration, time);
     }, noteData[0].repetition);
 
-    Tone.Transport.setInterval(function(time) {
-        synth2.triggerAttackRelease(noteData[1].note, noteData[1].duration, time);
-    }, noteData[1].repetition);
+    // Tone.Transport.setInterval(function(time) {
+    //     synth2.triggerAttackRelease(noteData[1].note, noteData[1].duration, time);
+    // }, noteData[1].repetition);
 
-    Tone.Transport.setInterval(function(time) {
-        synth1.triggerAttackRelease(noteData[2].note, noteData[2].duration, time);
-    }, noteData[2].repetition);
+    // Tone.Transport.setInterval(function(time) {
+    //     synth1.triggerAttackRelease(noteData[2].note, noteData[2].duration, time);
+    // }, noteData[2].repetition);
 
     //start the transport
     Tone.Transport.start();
