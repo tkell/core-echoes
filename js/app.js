f5eeda059ff301846f09740efc126f623ba08996
tkell <thor.kell@mail.mcgill.ca>
2015-02-28 14:22:19 -0500

    Trips synths

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
-var synth = new Tone.MonoSynth();
+var synth1 = new Tone.MonoSynth();
+var synth2 = new Tone.MonoSynth();
+var synth3 = new Tone.MonoSynth();
 
-//connect the synth to the master output channel
-synth.toMaster();
+var tempo = 128;
+var beatTime = 60 / 128;
 
-//create a callback which is invoked every quarter note
+synth1.setOscType('triangle');
+synth2.setOscType('square');
+synth3.setOscType('sine');
+
+// More synth quality here
+
+synth1.toMaster();
+synth2.toMaster();
+synth3.toMaster();
+
+Tone.Transport.setInterval(function(time) {
+    synth1.triggerAttackRelease("C4", 0.5, time);
+}, 1);
+
 Tone.Transport.setInterval(function(time){
-    //trigger middle C for the duration of an 8th note
-    synth.triggerAttackRelease("C4", "8n", time);
-}, "4n");
+    synth2.triggerAttackRelease("G4", 1.75, time);
+}, 3);
+
+Tone.Transport.setInterval(function(time){
+    synth3.triggerAttackRelease("C5", 0.75, time);
+}, 4);
 
 //start the transport
 Tone.Transport.start();
