f5e13708254dcf2b3d2df4e49a70247e6b4b1009
tkell <thor.kell@mail.mcgill.ca>
2015-02-28 18:18:13 -0500

    Colours and lights

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,27 @@
 var coreEchoesApp = angular.module('coreEchoesApp', []);
 
-coreEchoesApp.controller('echoController', function ($scope, $timeout) {
-    $scope.echoes = [
-        {'ip': '192.168.1.10'},
-        {'ip': '121.17.19.10'},
-        {'ip': '21.7.149.107'},
-        {'ip': '21.34.149.231'},
-        {'ip': '64.10.222.56'},
-    ];
+var testRoute = [
+    {'ip': '10.22.12.1', 'text': '10.22.12.1 19.811 ms  25.416 ms  28.152 ms'}, 
+    {'ip': '74.201.254.125', 'text': 'border2.ge4-28.spotify-3.nyj001.pnap.net 28.161 ms  28.119 ms  28.123 ms'}, 
+    {'ip': '216.52.95.88', 'text':  'core1.te6-2-bbnet2.nym007.pnap.net 64.714 ms  64.668 ms  64.676 ms'},
+    {'ip': '207.239.87.93', 'text': '207.239.87.93 44.890 ms  28.026 ms  50.806 ms'}, 
+    {'ip': '216.156.0.17', 'text': 'vb1010.rar3.nyc-ny.us.xo.net  64.591 ms  64.578 ms  64.562 ms'}, 
+    {'ip': '207.88.14.178', 'text': '207.88.14.178.ptr.us.xo.net 27.895 ms  12.237 ms  3.546 ms'},
+    {'ip': '154.54.11.189', 'text': 'te0-0-0-4.ccr21.jfk05.atlas.cogentco.com   11.343 ms  11.294 ms  11.239 ms'}, 
+    {'ip': '154.54.31.9', 'text': 'be2060.ccr41.jfk02.atlas.cogentco.com 11.188 ms  11.129 ms  5.606 ms'}, 
+    {'ip': '154.54.80.162', 'text': 'be2518.mpd21.dca01.atlas.cogentco.com 11.150 ms be2148.ccr21.dca01.atlas.cogentco.com (154.54.31.117)  11.126 ms be2518.mpd21.dca01.atlas.cogentco.com (154.54.80.162)  11.059 ms'},
+    {'ip': '154.54.31.97', 'text': 'be2169.ccr42.atl01.atlas.cogentco.com 27.448 ms be2170.ccr41.atl01.atlas.cogentco.com (154.54.31.105)  27.426 ms be2169.ccr42.atl01.atlas.cogentco.com (154.54.31.97)  27.341 ms'}, 
+    {'ip': '154.54.29.18', 'text': 'be2172.ccr21.iah01.atlas.cogentco.com 41.541 ms be2173.ccr22.iah01.atlas.cogentco.com (154.54.29.118)  37.263 ms be2172.ccr21.iah01.atlas.cogentco.com (154.54.29.18)  45.683 ms'}, 
+    {'ip': '154.54.5.66', 'text': 'be2065.ccr21.lax01.atlas.cogentco.com 73.413 ms be2066.ccr22.lax01.atlas.cogentco.com (154.54.7.54)  71.180 ms  79.183 ms'},
+    {'ip': '154.54.28.146', 'text': 'te8-1.mag01.lax01.atlas.cogentco.com 75.321 ms te7-1.mag02.lax01.atlas.cogentco.com (154.54.47.166)  72.894 ms  72.796 ms'}, 
+    {'ip': '154.24.22.122', 'text': 'te0-0-2-2.nr11.b001202-4.lax01.atlas.cogentco.com 72.832 ms  72.830 ms te0-0-2-1.nr11.b001202-4.lax01.atlas.cogentco.com (154.24.22.126)  72.780 ms'}, 
+    {'ip': '38.122.20.218', 'text': '38.122.20.218 72.709 ms  76.311 ms  76.211 ms'},
+    {'ip': '69.163.188.170', 'text': 'ip-69-163-188-170.dreamhost.com 76.194 ms ip-69-163-188-168.dreamhost.com (69.163.188.168)  73.629 ms ip-69-163-188-170.dreamhost.com (69.163.188.170)  76.145 ms'}, 
+    {'ip': '69.163.188.139', 'text': 'ip-69-163-188-139.dreamhost.com 76.147 ms  76.100 ms ip-69-163-188-137.dreamhost.com (69.163.188.137)  76.041 ms'}
+]
 
+coreEchoesApp.controller('echoController', function ($scope, $timeout) {
+    $scope.echoes = testRoute;
     $scope.showText = "co.re.ech.oes";
 
     $scope.startTimeout = function() {
@@ -21,8 +34,8 @@ coreEchoesApp.controller('echoController', function ($scope, $timeout) {
     var synth3 = new Tone.MonoSynth();
 
     synth1.setOscType('sine');
-    synth2.setOscType('sine');
-    synth3.setOscType('sine');
+    synth2.setOscType('triangle');
+    synth3.setOscType('square');
 
     // More synth quality here!
     synth1.output.gain.value = 0.1;
@@ -135,12 +148,37 @@ coreEchoesApp.controller('echoController', function ($scope, $timeout) {
         return times;   
     }
 
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
+
+    function rgbToHex(r, g, b) {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+
+    var getRGB = function(ipArray) {
+        ipString = ipArray[0] + ipArray[1] + ipArray[2] + ipArray[3];
+        ipString = ipString.replace('.', '');
+        console.log(ipString);
+        var r = parseInt(ipString.slice(0, 4)) % 255;
+        var g = parseInt(ipString.slice(5, 8)) % 255;
+        var b = parseInt(ipString.slice(9, 12)) % 255;
+        return rgbToHex(r, g, b)
+    }
+
     var doNextTimeout = function() {
         var currentIP = $scope.echoes[tempIndex].ip;
-        $scope.showText = currentIP;
+        $scope.showText = $scope.echoes[tempIndex].text;
+        $scope.showDetails = currentIP;
         var synthIndex = tempIndex % 3;
         currentIP = currentIP.split('.');
         currentIP = padIPs(currentIP);
+
+        console.log(getRGB(currentIP));
+        $scope.ipStyle = {
+            'background-color': getRGB(currentIP)
+        };
 
         var pitchProb = getProbabilties(currentIP[0]);
         var pitches = getPitches(currentIP.slice(1, 4));
@@ -173,7 +211,7 @@ coreEchoesApp.controller('echoController', function ($scope, $timeout) {
 
             if (synthIndex == 1) {  
                 synth1.output.gain.value = 0.3;
-                synth2.output.gain.value = 0.3;
+                synth2.output.gain.value = 0.2;
                 Tone.Transport.setInterval(function(time) {
                     synth2.triggerAttackRelease(noteData[1].note, noteData[1].duration, time);
                 }, noteData[1].repetition);
@@ -181,9 +219,9 @@ coreEchoesApp.controller('echoController', function ($scope, $timeout) {
 
             if (synthIndex == 2) {  
                 startFlag = false;
-                synth1.output.gain.value = 0.2;
+                synth1.output.gain.value = 0.4;
                 synth2.output.gain.value = 0.2;
-                synth3.output.gain.value = 0.2;
+                synth3.output.gain.value = 0.1;
                 Tone.Transport.setInterval(function(time) {
                     synth3.triggerAttackRelease(noteData[2].note, noteData[2].duration, time);
                 }, noteData[2].repetition);
